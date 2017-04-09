d2a779461683d7e429c2e6ce3c5064bb3890c269
Mario Di Vece <mario@unosquare.com>
2017-04-08 22:07:16 -0500

    minor documentation

--- Unosquare.LedEmotion.Controller/IWorker.cs
+++ Unosquare.LedEmotion.Controller/IWorker.cs
@@ -1,14 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Unosquare.LedEmotion.Controller
+﻿namespace Unosquare.LedEmotion.Controller
 {
+    /// <summary>
+    /// A simple interface for application workers
+    /// </summary>
     public interface IWorker
     {
+        /// <summary>
+        /// Should start the task immediately and asynchronously
+        /// </summary>
         void Start();
+        /// <summary>
+        /// Should stop the task immediately and synchronously
+        /// </summary>
         void Stop();
     }
 }
